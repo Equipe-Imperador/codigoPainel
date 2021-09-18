4e8c9e899fd9a7b8a60313adca9c4d5a98579116
Alexandre Schroeder <57239825+alexandrekj@users.noreply.github.com>
2021-09-18 19:20:59 -0300

    Código exemplo master I2C

---
+++ masterI2C.ino
@@ -0,0 +1,33 @@
+// Comunicação com o Painel via I2C
+// Exemplo de fragmento do código a ser executado pelo master do barramento (somente escrita no slave)
+
+const byte endSlave = 8; //endereço do device slave
+char buff[5]=""; //string de envio
+
+#include <Wire.h>
+
+void setup() 
+{  
+  Wire.begin();
+}
+
+void loop()
+{
+  Wire.beginTransmission(endSlave); // transmite para o device #8 (Painel)
+  // simulando envio de velocidade
+  Wire.write("v");        
+  Wire.write(leituraVelocidade);   
+  Wire.write(";"); //sinalizar fim da transmissão
+  
+  Wire.endTransmission();    // encerra transmissão
+
+  delay(500); 
+}
+
+int leituraVelocidade() //simulando tratamento de dados para serem enviados via I2C
+{
+  // parte do codigo de leitura da velocidade
+  int velocidade = 129; //exemplo
+
+  return velocidade;  
+}
